fcfbb29e6c35126e2bf991b647377b9bdf793838
Victor Gabriel Rabelo do Nascimento <108441696+victorRabel@users.noreply.github.com>
2023-10-12 20:10:53 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        //alert('Elemento não encontrado');
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
+
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code === 'Space' || evento.code === 'Enter') {
+            tecla.classList.add('ativa');
+        }
+
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const buttons = document.querySelectorAll(".tecla");
+    const audioElements = document.querySelectorAll("audio");
+
+    // Inicialmente, desative todos os áudios
+    audioElements.forEach(audio => audio.pause());
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function() {
+            const audioId = this.getAttribute("class").split(" ")[1];
+            const audioElement = document.getElementById("som_" + audioId);
+
+            // Pausar todos os áudios
+            audioElements.forEach(audio => audio.pause());
+
+            // Iniciar a reprodução do áudio associado ao botão clicado
+            audioElement.play();
+        });
+    });
+});
+
